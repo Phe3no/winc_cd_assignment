2b29f8ba31c5dfded8d4827e6e52b1645404ecb5
phe3no <phe3no@gmail.com>
2023-08-18 23:10:21 +0200

    SK changed

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ import os
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, 'app.db'),
     )
 
