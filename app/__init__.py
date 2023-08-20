e50f0418c7cd99acd16781c45973b42e42e1b229
phe3no <phe3no@gmail.com>
2023-08-20 12:05:11 +0200

    added an extra exception in public views registery bp

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ def create_app(test_config=None):
 
     if test_config is False:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
+        app.config.from_mapping("config.py", silent=True)
     else:
         # load the test congig if passed in
         app.config.from_mapping(test_config)
